63197b46f7bd25ae0be1d568c9ab2781398d49a9
Kevaron <kevtapia75@gmail.com>
2022-08-09 10:46:49 -0500

    adding bootstrap modal

--- src/app/modules/card/components/Sharer.vue
+++ src/app/modules/card/components/Sharer.vue
@@ -2,10 +2,26 @@
   <div class="sharer">
     <button
         class="sharer__button"
+        data-bs-toggle="modal" 
+        data-bs-target="#exampleModal"
         :style="sucursalColor">
         <i class="bi bi-share-fill icon"></i>
         Compartir
     </button>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Compartir vcard</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+    </div>
+  </div>
+</div>
+
   </div>
 </template>
 
