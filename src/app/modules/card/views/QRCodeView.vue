fee9b15eb8954d0f65bbb8151d85fe6dce244ca3
dimedical-corporativo <redessociales@dimedicalcorporativo.mx>
2024-04-03 12:45:34 -0600

    fix(qr): adding new library

--- src/app/modules/card/views/QRCodeView.vue
+++ src/app/modules/card/views/QRCodeView.vue
@@ -1,43 +1,27 @@
 <template>
     <div class="card-qr">
         <div class="card-qr__image">
+          <QrCode :value="currentLink" :foreground="backgroundColor" :size="350"/>
         </div>
-
-        <button
-            class="card-qr__download"
-            @click="downloadImage"
-            :style="{ backgroundColor: dotsColor }"
-        >
-            Descargar
-        </button>
-
     </div>
 </template>
 
 <script>
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
-import { surBase64, corpBase64 } from '../helpers/base64Logos'
+import QrCode from 'qrcode.vue'
 
 export default {
   setup () {
     const { params } = useRoute()
     const { sucursal } = params
-    const downloadImage = () => {
-      const imageBase64 = document.getElementsByClassName('img-qr')[0]
-      const linkUrl = document.createElement('a')
-      linkUrl.href = imageBase64.src
-      linkUrl.download = `${params.id}.jpg`
-      linkUrl.click()
-    }
-
     return {
       currentLink: computed(() => `https://card.dimedicalcorporativo.mx/${params.sucursal}/${params.id}`),
-      dotsColor: computed(() => sucursal === 'sur' ? '#218d9b' : '#c28400'),
-      cornersCOlor: computed(() => sucursal === 'sur' ? '#187876' : '#684700'),
-      downloadImage,
-      base64Image: computed(() => sucursal === 'sur' ? surBase64 : corpBase64)
+      backgroundColor: computed(() => sucursal === 'sur' ? '#218d9b' : '#c28400')
     }
+  },
+  components: {
+    QrCode
   }
 }
 </script>
